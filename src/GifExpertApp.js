687f933bbd1a6bc6d207a350a9421b5b385f6415
Rafael Jimenez <rjimenezb20@gmail.com>
2021-09-14 14:18:54 +0200

    Updated

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useState } from 'react/cjs/react.development';
+import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
 
